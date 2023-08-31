1097be88b309f315f7dd3990fc8666d43075e1e7
Tolgaycode <tolgaycode@gmail.com>
2023-08-31 19:34:36 +0300

    Readme added

---
+++ README.md
@@ -0,0 +1,51 @@
+# Coingecko Search Input Project
+
+This is a simple web application that utilizes the Coingecko API to create a search input that displays coin names and their logos.
+
+## Project Description
+
+The Coingecko Search Input project is a web application that allows users to search for coin names using a search input. As the user types, the application dynamically fetches coin data from the Coingecko API and displays matching coin names along with their logos. When a user clicks on a coin name from the dropdown, the application updates to show the selected coin's name and logo.
+
+## Features
+
+- User-friendly interface with a search input and dynamic dropdown.
+- Fetches coin data from the Coingecko API based on user input.
+- Displays matching coin names and logos in the dropdown.
+- Allows users to select a coin from the dropdown to see its name and logo.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Coingecko API
+
+## How to Use
+
+1. Clone this repository to your local machine using:
+
+```sh
+git clone https://github.com/Tolgaycode/coingecko-search-input.git
+```
+
+2. Open the project directory:
+
+```sh
+cd coingecko-search-input
+```
+
+3. Open the `index.html` file in your web browser to use the application.
+
+4. Start typing in the search input to see matching coin names and logos in the dropdown. Click on a coin name to display its name and logo in the results section.
+
+## Live Demo
+
+You can check out the live demo of the project [here](https://Tolgaycode.github.io/coingecko-search-input).
+
+## Acknowledgements
+
+- This project uses the [Coingecko API](https://www.coingecko.com/api/documentation).
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- app.js
+++ app.js
@@ -1,40 +1,51 @@
+// Get references to HTML elements
 const searchInput = document.getElementById("searchInput");
 const dropdownList = document.getElementById("dropdownList");
 const coinNameElement = document.getElementById("coinName");
 const coinLogoElement = document.getElementById("coinLogo");
 
+// Declare a variable for timeout ID
 let timeoutId;
 
+// Listen for input changes in the search input field
 searchInput.addEventListener("input", () => {
   const searchTerm = searchInput.value.toLowerCase().trim();
 
+  // Clear any existing timeouts
   clearTimeout(timeoutId);
+
+  // If search input is empty, close the dropdown and return
   if (searchTerm === "") {
     closeDropdown();
     return;
   }
 
+  // Set a timeout to fetch coin data after a brief delay
   timeoutId = setTimeout(() => {
-    getCoindata(searchTerm);
+    getCoinData(searchTerm);
   }, 300);
 });
 
+// Listen for clicks on the document to close the dropdown when clicking outside
 document.addEventListener("click", (event) => {
   if (!searchInput.contains(event.target)) {
     closeDropdown();
   }
 });
 
-async function getCoindata(coin) {
+// Fetch coin data from the API
+async function getCoinData(coin) {
   const url = `https://api.coingecko.com/api/v3/search?query=${coin}`;
   const response = await fetch(url);
   const data = await response.json();
 
   const coins = data.coins.slice(0, 10);
 
+  // Render the dropdown with fetched coin data
   renderDropdown(coins);
 }
 
+// Render the dropdown list with coin data
 function renderDropdown(coins) {
   dropdownList.innerHTML = "";
 
@@ -45,6 +56,7 @@ function renderDropdown(coins) {
       <span>${coin.name}</span>
     `;
 
+    // Handle click on a dropdown item
     li.addEventListener("click", () => {
       searchInput.value = coin.name;
       coinNameElement.textContent = "Coin Name: " + coin.name;
@@ -56,6 +68,7 @@ function renderDropdown(coins) {
     dropdownList.appendChild(li);
   });
 
+  // Display "No results found" if there are no matching coins
   if (coins.length === 0) {
     const li = document.createElement("li");
     li.textContent = "No results found";
@@ -63,6 +76,7 @@ function renderDropdown(coins) {
   }
 }
 
+// Close the dropdown by clearing its content
 function closeDropdown() {
   dropdownList.innerHTML = "";
 }
