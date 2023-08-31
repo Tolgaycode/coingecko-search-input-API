5d05db2bc8aca348f328979569f0233364f33a66
Tolgaycode <tolgaycode@gmail.com>
2023-08-31 19:11:07 +0300

    updated

--- app.js
+++ app.js
@@ -1,42 +1,68 @@
 const searchInput = document.getElementById("searchInput");
 const dropdownList = document.getElementById("dropdownList");
-const items = [
-  "Apple",
-  "Banana",
-  "Cherry",
-  "Date",
-  "Grape",
-  "Lemon",
-  "Mango",
-  "Orange",
-  "Pineapple",
-];
+const coinNameElement = document.getElementById("coinName");
+const coinLogoElement = document.getElementById("coinLogo");
+
+let timeoutId;
 
 searchInput.addEventListener("input", () => {
-  const searchTerm = searchInput.value.toLowerCase();
-  const filteredItems = items.filter((item) =>
-    item.toLowerCase().includes(searchTerm)
-  );
+  const searchTerm = searchInput.value.toLowerCase().trim();
 
-  renderDropdown(filteredItems);
+  clearTimeout(timeoutId);
+  if (searchTerm === "") {
+    closeDropdown();
+    return;
+  }
+
+  timeoutId = setTimeout(() => {
+    getCoindata(searchTerm);
+  }, 300);
 });
 
-function renderDropdown(filteredItems) {
+document.addEventListener("click", (event) => {
+  if (!searchInput.contains(event.target)) {
+    closeDropdown();
+  }
+});
+
+async function getCoindata(coin) {
+  const url = `https://api.coingecko.com/api/v3/search?query=${coin}`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const coins = data.coins.slice(0, 10);
+
+  renderDropdown(coins);
+}
+
+function renderDropdown(coins) {
   dropdownList.innerHTML = "";
 
-  if (filteredItems.length > 0) {
-    filteredItems.forEach((item) => {
-      const li = document.createElement("li");
-      li.textContent = item;
-      li.addEventListener("click", () => {
-        searchInput.value = item;
-        dropdownList.innerHTML = "";
-      });
-      dropdownList.appendChild(li);
+  coins.forEach((coin) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <img src="${coin.thumb}" alt="${coin.name} Logo">
+      <span>${coin.name}</span>
+    `;
+
+    li.addEventListener("click", () => {
+      searchInput.value = coin.name;
+      coinNameElement.textContent = "Coin Name: " + coin.name;
+      coinLogoElement.innerHTML =
+        "Coin Logo: " + `<img src="${coin.thumb}" alt="${coin.name} Logo">`;
+      closeDropdown();
     });
-  } else {
+
+    dropdownList.appendChild(li);
+  });
+
+  if (coins.length === 0) {
     const li = document.createElement("li");
     li.textContent = "No results found";
     dropdownList.appendChild(li);
   }
+}
+
+function closeDropdown() {
+  dropdownList.innerHTML = "";
 }
